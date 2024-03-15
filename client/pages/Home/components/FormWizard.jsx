0f3543535d85c5dbec10998d0b796ecc1a4082d7
Jonathan Benn <bonjennprojects@gmail.com>
2024-03-14 17:56:55 -0700

    Navigate from signUpForm1 to signUpForm3 if you've already signed up

--- client/pages/Home/components/FormWizard.jsx
+++ client/pages/Home/components/FormWizard.jsx
@@ -15,9 +15,23 @@ const FormWizard = () => {
     const [password, setPassword] = useState(''); 
     const [formName, setFormName] = useState(''); // Add this line
     const [customDomain, setCustomDomain] = useState(''); // Add this line
+    const [lastStep, setLastStep] = useState(null); // Add this line
+    const [hasSignedUp, setHasSignedUp] = useState(false); // Add this line
 
-    const nextStep = () => setCurrentStep(currentStep + 1);
-    const prevStep = () => setCurrentStep(currentStep - 1);
+    const nextStep = () => {
+        if (currentStep === 1 && lastStep) {
+            setCurrentStep(lastStep);
+            setLastStep(null); // Reset lastStep after navigating
+        } else {
+            setCurrentStep(currentStep + 1);
+        }
+    };
+    const prevStep = () => {
+        if (currentStep > 2) {
+            setLastStep(currentStep - 1);
+        }
+        setCurrentStep(currentStep - 1);
+    };
 
     const updateFormId = (id) => {
         setFormId(id);
@@ -32,9 +46,13 @@ const FormWizard = () => {
             case 1:
                 return <SignUpForm1 onNext={nextStep} updateFormId={updateFormId} formId={formId} formName={formName} setFormName={setFormName} customDomain={customDomain} setCustomDomain={setCustomDomain} />;
             case 2:
-                return <SignUpForm2 onNext={nextStep} onBack={prevStep} email={email} setEmail={setEmail} password={password} setPassword={setPassword} />;
+                if (hasSignedUp) {
+                    return <SignUpForm3 onNext={nextStep} onBack={() => setCurrentStep(1)} formId={formId} additionalFields={additionalFields} updateAdditionalFields={updateAdditionalFields} infoType={infoType} setInfoType={setInfoType} />;
+                } else {
+                    return <SignUpForm2 onNext={() => { setHasSignedUp(true); nextStep(); }} onBack={prevStep} email={email} setEmail={setEmail} password={password} setPassword={setPassword} />;
+                }
             case 3:
-                return <SignUpForm3 onNext={nextStep} onBack={() => setCurrentStep(1)} formId={formId} additionalFields={additionalFields} updateAdditionalFields={updateAdditionalFields} infoType={infoType} setInfoType={setInfoType} />;
+                return <SignUpForm3 onNext={nextStep} onBack={() => { setLastStep(3); setCurrentStep(1); }} formId={formId} additionalFields={additionalFields} updateAdditionalFields={updateAdditionalFields} infoType={infoType} setInfoType={setInfoType} />;
             case 4:
                 return <SignUpForm4 onNext={nextStep} onBack={prevStep} formId={formId} />;
             case 5:
