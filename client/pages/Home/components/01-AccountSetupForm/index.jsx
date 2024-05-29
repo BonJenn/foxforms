59ab058af2aec588ec9a1331fd268ad54cdf37b2
Bon Jenn <133067965+BonJenn@users.noreply.github.com>
2024-05-29 06:56:11 -0700

    Redesigning homepage

--- client/pages/Home/components/01-AccountSetupForm/index.jsx
+++ client/pages/Home/components/01-AccountSetupForm/index.jsx
@@ -55,19 +55,10 @@ const AccountSetupForm = ({ onBack, onNext, username, setUsername, password, set
                     <div className={styles.AccountSetupFormHero}>
                         <h1><span className={styles.gradientText}>Effortless</span> form building.</h1>
                         <h1><span className={styles.gradientText}>Seamless</span> data collection.</h1>
+                        <h3> Gather customer insights with precision and ease.</h3>
                     </div>
 
-                    <div className={styles.signUpForm2}>
-                        <h1>Sign Up</h1>
-                        <form onSubmit={handleSubmit}>
-                            <input type="email" id="username" name="username" value={username} required onChange={handleChange} placeholder="Username" />
-                            <input type="password" id="password" name="password" value={password} required onChange={handleChange} placeholder="Password" />
-                            <input type="password" id="confirmPassword" name="confirmPassword" required onChange={handleChange} placeholder="Confirm Password" />
-                            {passwordError && <div className={styles.passwordError}>{passwordError}</div>}
-                            <button type="submit">Sign Up</button>
-                            <button type="button" onClick={onBack}>Back</button>
-                        </form>
-                    </div>
+                   
                 </div>
 
                 <div className={styles.accountSetupFormSect2}>
